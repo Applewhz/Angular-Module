312d028ad02c6e271fb5fd76ddcdb9047c2d5666
DarrenWong <darren.wong@hkjc.org.hk>
2023-02-12 17:45:30 +0800

    task 1 completed task2 ongoing

---
+++ courses-app/src/app/shared/components/button/button.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.scss']
+})
+export class ButtonComponent{
+  @Input() text?: string;
+  // buttonTitle: string = 'something?'
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas);
+  }
+
+  ngOnInit(): void {
+
+  }
+}
---
+++ courses-app/src/app/shared/shared.module.ts
@@ -0,0 +1,39 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ModalComponent } from './components/modal/modal.component';
+import {
+  HeaderComponent,
+  ButtonComponent,
+  InfoComponent,
+  SearchComponent,
+  CourseCardComponent,
+  LoginFormComponent,
+  RegistrationFormComponent,
+  CourseFormComponent
+} from "./components";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+
+const COMPONENTS = [
+  HeaderComponent,
+  ButtonComponent,
+  InfoComponent,
+  SearchComponent,
+  ModalComponent,
+  CourseCardComponent,
+  LoginFormComponent,
+  RegistrationFormComponent,
+  CourseFormComponent
+];
+
+@NgModule({
+  declarations: [COMPONENTS],
+  imports: [
+    CommonModule,
+    FontAwesomeModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  exports: [COMPONENTS]
+})
+export class SharedModule { }
