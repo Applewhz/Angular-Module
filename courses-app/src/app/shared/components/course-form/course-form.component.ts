e07330062e7f3b372b692619bc411ae371c4c28a
Applewhz <76094766+Applewhz@users.noreply.github.com>
2023-02-20 02:10:14 +0800

    module 3 task 4 title validation implemented but bugged

--- courses-app/src/app/shared/components/course-form/course-form.component.ts
+++ courses-app/src/app/shared/components/course-form/course-form.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import {
-  FormBuilder
+  FormBuilder, Validators
 } from '@angular/forms';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -11,7 +11,31 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./course-form.component.scss'],
 })
 export class CourseFormComponent {
+
+  courseForm: any
+  show = false
+
   constructor(public fb: FormBuilder, public library: FaIconLibrary) {
     library.addIconPacks(fas);
+    this.courseForm = this.fb.group({
+      title: ['', [Validators.required, Validators.minLength(10)]],
+      description: ['', [Validators.required, Validators.minLength(10)]],
+      authors: [''],
+      duration: ['', [Validators.required, Validators.min(0)]],
+    })
+  }
+
+  onSubmit(value:any) {
+    // this.submitted = true
+    console.log('here', this.show)
+    console.log(value)
+  }
+
+  onCancel() {
+    console.log('GO BACK')
+  }
+
+  get courseFormControl() {
+    return this.courseForm.controls;
   }
 }
