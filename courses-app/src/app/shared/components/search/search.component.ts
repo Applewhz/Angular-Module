085aeb51b0260b6862d6a8009108fbd60d4f1f88
Applewhz <76094766+Applewhz@users.noreply.github.com>
2023-02-22 03:00:10 +0800

    module 3 task 6 completed

--- courses-app/src/app/shared/components/search/search.component.ts
+++ courses-app/src/app/shared/components/search/search.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
-export class SearchComponent {}
+export class SearchComponent implements OnInit{
+  @Input() SearchString!: string
+  @Output() InputSearchString = new EventEmitter<string>();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  userInputString(value: string) {
+    this.InputSearchString.emit(value);
+    console.log('here', value)
+  }
+}
 
