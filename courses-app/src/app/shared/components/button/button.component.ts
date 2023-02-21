80ac22d7cdd05e60e6c7436295be0cbb3df7231a
Applewhz <76094766+Applewhz@users.noreply.github.com>
2023-02-22 01:56:06 +0800

    module 3 task 4 completed

--- courses-app/src/app/shared/components/button/button.component.ts
+++ courses-app/src/app/shared/components/button/button.component.ts
@@ -1,6 +1,6 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
-import { fas, faTrashCan, faPencil } from '@fortawesome/free-solid-svg-icons';
+import { fas, faTrashCan, faPencil, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-button',
@@ -11,9 +11,11 @@ export class ButtonComponent implements OnInit{
   @Input() text?: string;
   @Input() showDeleteIcon?: boolean;
   @Input() showEditIcon?: boolean;
+  @Input() showCancelIcon?: boolean;
   @Output() btnClick = new EventEmitter()
   delete = faTrashCan
   edit = faPencil
+  cancel = faXmark
   constructor(library: FaIconLibrary) {
     library.addIconPacks(fas);
   }
--- courses-app/src/app/shared/components/course-form/course-form.component.ts
+++ courses-app/src/app/shared/components/course-form/course-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {
+  FormArray,
   FormBuilder, Validators
 } from '@angular/forms';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
@@ -20,14 +21,23 @@ export class CourseFormComponent {
     this.courseForm = this.fb.group({
       title: ['', [Validators.required, Validators.minLength(10)]],
       description: ['', [Validators.required, Validators.minLength(10)]],
-      authors: [''],
-      duration: ['', [Validators.required, Validators.min(0)]],
+      authors: this.fb.array(['Author 1', 'Author 2']),
+      duration: [0, [Validators.required, Validators.min(0)]],
     })
   }
-
+  
+  // ngOnInit() {
+  //   this.courseForm = this.fb.group({
+  //     title: ['', [Validators.required, Validators.minLength(10)]],
+  //     description: ['', [Validators.required, Validators.minLength(10)]],
+  //     authors: this.fb.array(['Author 1', 'Author 2']),
+  //     duration: ['', [Validators.required, Validators.min(0)]],
+  //   })
+  // }
   onSubmit(value:any) {
     // this.submitted = true
-    console.log('here', this.show)
+    // console.log('here', this.show)
+    // console.log('here',this.authorsArray.get('authors'))
     console.log(value)
   }
 
@@ -35,7 +45,27 @@ export class CourseFormComponent {
     console.log('GO BACK')
   }
 
+  onCancelAuthor() {
+    console.log('Delete author?')
+  }
+
+  getHours = (duration: number) => {
+    const value =  duration / 60
+    if(value < 10){
+      return '0' + Math.floor(value)
+    }
+    return Math.floor(value)
+  }
+  getMinutes = (duration: number) => {
+    const value =  duration % 60
+    if(value < 10){
+      return '0' + Math.floor(value)
+    }
+    return Math.floor(value)
+  }
+
   get courseFormControl() {
     return this.courseForm.controls;
   }
+
 }
