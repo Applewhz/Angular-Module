1c574b461ac4855873c47b36003b95a51efe8707
Applewhz <76094766+Applewhz@users.noreply.github.com>
2023-02-24 03:38:06 +0800

    module 4 task 1 completed

--- courses-app/src/app/app-routing.module.ts
+++ courses-app/src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CourseComponent } from './features/course/course.component';
+import { CoursesComponent } from './features/courses/courses.component';
 import { LoginComponent } from './features/login/login.component';
 import { RegistrationComponent } from './features/registration/registration.component';
 
 const routes: Routes = [
-  { path: '/login', component: LoginComponent },
-  { path: '/registration', component: RegistrationComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registration', component: RegistrationComponent },
+  { path: 'courses', component: CoursesComponent },
+  { path: 'courses/add', component: CourseComponent },
+  { path: 'courses/:id', component: CoursesComponent },
+  { path: 'courses/edit/:id', component: CoursesComponent },
+  { path: '',   redirectTo: '/courses', pathMatch: 'full' },
 ];
 
 @NgModule({
